f65a689afcf55d0b4edc74c9a5c4bd5453270508
krtsato <ev60596@meiji.ac.jp>
2020-03-16 16:27:55 +0900

    feat: create erb template

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root to: 'top#index'
+  end
+  
+  namespace :staff do
+    root to: 'top#index'
+  end 
+  
+  namespace :customer do
+    root to: 'top#index'
+  end
 end
