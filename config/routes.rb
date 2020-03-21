f9f91d598c5d164f8ff596d9ed8f73e8f4c0dc98
krtsato <ev60596@meiji.ac.jp>
2020-03-21 14:10:45 +0900

    fix: login symbol typo

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   namespace :staff do
     root 'top#index'
-    get 'login' => 'sessions#new', as: login
+    get 'login' => 'sessions#new', as: :login
     resource :session, only: [:create, :destroy]
   end 
   
