43106aeb0b9e397b7455e9335c2c9a8cd8b18bb0
krtsato <ev60596@meiji.ac.jp>
2020-03-30 01:23:01 +0900

    fix: correct host and path

--- config/initializers/rrrp.rb
+++ config/initializers/rrrp.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.rrrp = {
-    admin: {host: ENV['ADMIN_STAFF_HOST_NAME'], path: ''},
-    staff: {host: ENV['ADMIN_STAFF_HOST_NAME'], path: 'admin'},
+    admin: {host: ENV['ADMIN_STAFF_HOST_NAME'], path: 'admin'},
+    staff: {host: ENV['ADMIN_STAFF_HOST_NAME'], path: ''},
     customer: {host: ENV['CUSTOMER_HOST_NAME'], path: 'mypage'}
   }
 end
