4581f64a3b404094f8dc09aebea50957d92f4c1e
krtsato <ev60596@meiji.ac.jp>
2020-04-01 12:15:39 +0900

    test: staff_account update

--- spec/factories/administrator.rb
+++ spec/factories/administrator.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :administrator do
     sequence(:email) {|n| "admin#{n}@example.com"}
-    password {'pw'}
+    password {'password'}
     suspended {false}
   end
 end
---
+++ spec/requests/staff/staff_account_management_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'Staff::Account', type: :request do
+  before do
+    post staff_session_url, params: {
+      staff_login_form: {email: staff_member.email, password: 'password'}
+    }
+  end
+
+  describe '情報表示' do
+    let(:staff_member) {create(:staff_member)}
+
+    example '成功' do
+      get staff_account_url
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe '更新' do
+    let(:params_hash) {attributes_for(:staff_member)}
+    let(:staff_member) {create(:staff_member)}
+
+    example 'email 属性を変更する' do
+      params_hash.merge!(email: 'test@example.com')
+      patch staff_account_url, params: {id: staff_member.id, staff_member: params_hash}
+      staff_member.reload # オブジェクトの各属性値を DB から再取得する
+      expect(staff_member.email).to eq('test@example.com')
+    end
+
+    example '例外 ActionController::ParameterMissing が発生' do
+      expect {patch staff_account_url, params: {id: staff_member.id}}.to \
+        raise_error(ActionController::ParameterMissing)
+    end
+
+    example 'end_date の値は書き換え不可' do
+      params_hash.merge!(end_date: Time.zone.tomorrow)
+      expect {patch staff_account_url, params: {id: staff_member.id, staff_member: params_hash}}.not_to \
+        change {staff_member.end_date}
+    end
+  end
+end
