9d94fa89525617fbb24006ced666609711b2f053
krtsato <ev60596@meiji.ac.jp>
2020-04-12 17:52:06 +0900

    feat: validation and normalization for staff members' name

---
+++ app/models/concerns/string_normalizer.rb
@@ -0,0 +1,17 @@
+require 'nkf'
+
+module StringNormalizer
+  extend ActiveSupport::Concern
+
+  def normalize_as_email(text)
+    NKF.nkf('-WwZ1', text).strip if text
+  end
+
+  def normalize_as_name(text)
+    NKF.nkf('-WwZ1', text).strip if text
+  end
+
+  def normalize_as_furigana(text)
+    NKF.nkf('-WwZ1 --katakana', text).strip if text
+  end
+end
