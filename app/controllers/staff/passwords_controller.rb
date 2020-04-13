00eacef04dac0964e75a33a557e7f4ee4e74f1b8
krtsato <ev60596@meiji.ac.jp>
2020-04-13 16:23:43 +0900

    feat: a staff member can change his own password

---
+++ app/controllers/staff/passwords_controller.rb
@@ -0,0 +1,31 @@
+module Staff
+  class PasswordsController < Base
+    def show
+      redirect_to :edit_staff_password
+    end
+
+    def edit
+      @change_password_form = ChangePasswordForm.new(object: current_staff_member)
+    end
+
+    def update
+      @change_password_form = ChangePasswordForm.new(staff_member_params)
+      @change_password_form.object = current_staff_member
+      if @change_password_form.save
+        flash.now.notice = 'パスワードを変更しました'
+        redirect_to :staff_account
+      else
+        flash.now.alert = '入力に誤りがあります'
+        render action: 'edit'
+      end
+    end
+
+    private
+
+    def staff_member_params
+      params.require(:staff_change_password_form).permit(
+        :current_password, :new_password, :new_password_confirmation
+      )
+    end
+  end
+end
