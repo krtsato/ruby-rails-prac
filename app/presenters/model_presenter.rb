204fe659e9c280f2d6ef7f876cd7723851707c67
krtsato <ev60596@meiji.ac.jp>
2020-04-14 18:10:28 +0900

    fix: rubocop correct and prepare html builder by nokogiri

--- app/presenters/model_presenter.rb
+++ app/presenters/model_presenter.rb
@@ -1,6 +1,10 @@
-class MedelPresenter
+# frozen_string_literal: true
+
+class ModelPresenter
+  include HtmlBuilder
   attr_reader :object, :view_context
-  delegate :raw, to: :view_context
+
+  delegate :sanitize, to: :view_context
 
   def initialize(object, view_context)
     @object = object
--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class StaffMemberPresenter < ModelPresenter
   delegate :suspended?, to: :object
 
   # 職員の停止フラグの On/Off を表現する記号を返す
   def suspended_mark
-    suspended? ? raw("&#x2611;") : raw("&#x2610;")
+    suspended? ? sanitize('&#x2611;') : sanitize('&#x2610;')
   end
 end
