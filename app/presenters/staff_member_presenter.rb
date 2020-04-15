68c419bc06a9d82ea6261223b43ade3525472bf2
krtsato <ev60596@meiji.ac.jp>
2020-04-15 15:34:17 +0900

    feat: add staff member's full name presenter

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class StaffMemberPresenter < ModelPresenter
-  delegate :suspended?, to: :object
+  delegate :suspended?, :family_name, :given_name, :family_name_kana, :given_name_kana, to: :object
+
+  def full_name
+    family_name + " " + given_name
+  end
+
+  def full_name_kana
+    family_name_kana + " " + given_name_kana
+  end
 
   # 職員の停止フラグの On/Off を表現する記号を返す
   def suspended_mark
