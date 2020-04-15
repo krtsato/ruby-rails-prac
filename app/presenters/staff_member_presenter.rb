77da1c707e7ba38b339f7b7e6ca6ff52cfb42b56
krtsato <ev60596@meiji.ac.jp>
2020-04-15 16:23:05 +0900

    fix: rubocop correct

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -4,11 +4,11 @@ class StaffMemberPresenter < ModelPresenter
   delegate :suspended?, :family_name, :given_name, :family_name_kana, :given_name_kana, to: :object
 
   def full_name
-    family_name + " " + given_name
+    family_name + ' ' + given_name
   end
 
   def full_name_kana
-    family_name_kana + " " + given_name_kana
+    family_name_kana + ' ' + given_name_kana
   end
 
   # 職員の停止フラグの On/Off を表現する記号を返す
--- lib/html_builder.rb
+++ lib/html_builder.rb
@@ -3,7 +3,7 @@
 module HtmlBuilder
   def markup(tag_name = nil, options = {})
     root = Nokogiri::HTML::DocumentFragment.parse('')
-    
+
     Nokogiri::HTML::Builder.with(root) do |doc|
       if tag_name
         doc.method_missing(tag_name, options) do
@@ -14,6 +14,6 @@ module HtmlBuilder
       end
     end
 
-    sanitize(root.to_html, tags: %w(a table th tr td))
+    sanitize(root.to_html, tags: %w[a table th tr td])
   end
 end
