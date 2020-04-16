3bf0a96a77f778c878c3a344d2903436c847e63c
krtsato <ev60596@meiji.ac.jp>
2020-04-16 19:33:32 +0900

    refactor: make common decorated label in staff member form presenter

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -7,11 +7,54 @@ class FormPresenter
   attr_reader :form_builder, :view_context
 
   delegate \
-    :label, :text_field, :date_field, :password_field, :check_box, :radio_button, :text_area, :object,
+    :label, :text_field, :date_field, :password_field, :check_box, :radio_button, :text_area, :sanitize, :object,
     to: :form_builder
 
+  # インスタンス化
   def initialize(form_builder, view_context)
     @form_builder = form_builder
     @view_context = view_context
+  end
+
+  # 入力必須
+  def notes
+    markup(:div, class: 'notes') do |m|
+      m.span '*', class: 'mark'
+      m.text '印の付いた項目は入力必須です。'
+    end
+  end
+
+  # 文字入力
+  def text_field_block(input, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << decorated_label(input, label_text, options)
+      m << text_field(input, options)
+      m << error_messages_for(input)
+    end
+  end
+
+  # パスワード
+  def password_field_block(input, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << decorated_label(input, label_text, options)
+      m << password_field(input, options)
+      m << error_messages_for(input)
+    end
+  end
+
+  # 日付
+  def date_field_block(input, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << decorated_label(input, label_text, options)
+      m << date_field(input, options)
+      m << error_messages_for(input)
+    end
+  end
+
+
+  private
+
+  def decorated_label(input, label_text, options = {})
+    label(input, label_text, class: options[:required] ? 'required' : nil)
   end
 end
---
+++ app/presenters/staff_member_form_presenter.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class StaffMemberFormPresenter < FormPresenter
+  # オーバーライド
+  def password_field_block(input, label_text, options = {})
+    if object.new_record?
+      super(name, label_text, options)
+    end
+  end
+
+  # 姓名
+  def full_name_block(f_name, g_name, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << decorated_label(f_name, label_text, options)
+      m << text_field(f_name, options)
+      m << text_field(g_name, options)
+    end
+  end
+
+  # アカウント停止
+  def suspended_check_box
+    markup(:div, class: 'check-boxes') do |m|
+      m << check_box(:suspended)
+      m << label(:suspended, 'アカウント停止')
+    end
+  end
+end
