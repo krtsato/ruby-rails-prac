a18c43c6ae7a1a6425562510566cd63f619b5be2
krtsato <ev60596@meiji.ac.jp>
2020-04-18 23:07:24 +0900

    feat: add gender and birthday validation to the customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,13 @@ class Customer < ApplicationRecord
   has_one :home_address, dependent: :destroy
   has_one :work_address, dependent: :destroy
 
+  validates :gender, inclusion: {in: %w[male female], allow_blank: true}
+  validates :birthday, date: {
+    after: Tiem.zone.local(1900, 1, 1).to_date,
+    before: -> (_obj) {Time.zone.today},
+    allow_blank: true
+  }
+
   def password=(raw_password)
     if raw_password.is_a?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
