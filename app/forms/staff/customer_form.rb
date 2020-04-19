27fc9c12d58f378627d7eb0593890cdb1938132b
krtsato <ev60596@meiji.ac.jp>
2020-04-19 16:14:16 +0900

    feat: control checkbox form process

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -10,15 +10,30 @@ module Staff
     def initialize(customer = nil)
       @customer = customer
       @customer ||= Customer.new(gender: 'male')
+      self.inputs_home_address = @customer.home_address.present?
+      self.inputs_work_address = @customer.work_address.present?
       @customer.build_home_address unless @customer.home_address
       @customer.build_work_address unless @customer.work_address
     end
 
     def assign_attributes(params = {})
       @params = params
+      self.inputs_home_address = params[:inputs_home_address] == '1' # true: 1, false: 0
+      self.inputs_work_address = params[:inputs_work_address] == '1' # true: 1, false, 0
+      
       customer.assign_attributes(customer_params)
-      customer.home_address.assign_attributes(home_address_params)
-      customer.work_address.assign_attributes(work_address_params)
+
+      if inputs_home_address
+        customer.home_address.assign_attributes(home_address_params)
+      else
+        customer.home_address.mark_for_destruction
+      end
+
+      if inputs_work_address
+        customer.work_address.assign_attributes(work_address_params)
+      else
+        customer.work_address.mark_for_destruction
+      end
     end
 
     private
---
+++ app/javascript/packs/staff.js
@@ -0,0 +1,4 @@
+require('@rails/ujs').start()
+require('@rails/activestorage').start()
+
+import '../staff/cusomer_form.js'
