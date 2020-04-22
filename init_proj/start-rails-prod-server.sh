cb7cfff856fde4a0d721521a9555f84c84999657
krtsato <ev60596@meiji.ac.jp>
2020-04-23 00:00:04 +0900

    fix: start server shell

--- init_proj/start-rails-prod-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-
-bundle exec rails db:migrate
-
-bundle exec rails assets:precompile
-
-bundle exec rails s -b '0.0.0.0' -p 3000
--- init_proj/start-rails-server.sh
+++ init_proj/start-rails-server.sh
@@ -4,6 +4,10 @@ set -x
 
 bundle check || bundle install -j4
 
+# if production mode
+# bundle exec rails db:migrate
+# bundle exec rails assets:precompile
+
 rm -f tmp/pids/server.pid
 
 bundle exec rails s -b '0.0.0.0' -p 3000
