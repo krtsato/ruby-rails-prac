29153b57d3b0b68327abb0c49558d2322579646a
krtsato <ev60596@meiji.ac.jp>
2020-05-20 11:39:17 +0900

    fix: remove a deprecated flag from bundle install

--- init_proj/deploy-setup.sh
+++ init_proj/deploy-setup.sh
@@ -6,7 +6,7 @@ cd ruby-rails-rspec-prac && mkdir -p tmp/{db,pids,sockets}
 
 docker-compose run --rm web bash -c \
 "set -x \
-&& bundle install -j4 --deployment \
+&& bundle install -j4 \
 && yarn install --production \
 && bundle exec rails db:create RAILS_ENV=production \
 && bundle exec rails db:migrate RAILS_ENV=production \
